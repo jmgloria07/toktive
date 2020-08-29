f17d3a1a6f84c1f8be8b9489f70f75145df43f99
jmgloria07 <joshua.gloria07@gmail.com>
2020-08-29 16:33:23 +0800

    Separate console client via command line reader for functional testing

--- src/main/java/io/github/jmgloria07/toktive/ToktiveApplication.java
+++ src/main/java/io/github/jmgloria07/toktive/ToktiveApplication.java
@@ -1,25 +1,13 @@
 package io.github.jmgloria07.toktive;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import io.github.jmgloria07.toktive.business.ToktiveService;
-import io.github.jmgloria07.toktive.business.ToktiveServiceImpl;
 
 @SpringBootApplication
 public class ToktiveApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext appContext = SpringApplication.run(ToktiveApplication.class, args);
-		
-		//if (startInConsole)
-		ToktiveService service = appContext.getBean(ToktiveServiceImpl.class);
-		
-		service.share("test share post", new HashSet<>(Arrays.asList("TW","FB")));
+		SpringApplication.run(ToktiveApplication.class, args);
 	}
 
 }
