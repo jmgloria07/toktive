e1e84acfdc67cb86d555c1a8859858696c033575
jmgloria07 <joshua.gloria07@gmail.com>
2020-09-09 19:36:55 +0800

    v6.0.2 Additional Social Network enum data

--- src/main/java/io/github/jmgloria07/toktive/api/objects/SocialNetwork.java
+++ src/main/java/io/github/jmgloria07/toktive/api/objects/SocialNetwork.java
@@ -2,17 +2,26 @@ package io.github.jmgloria07.toktive.api.objects;
 
 public enum SocialNetwork {
 	
-	FB(true),
-	TW(false),
-	FB_PAGES(false);
+	FB(true, "Facebook"),
+	TW(false, "Twitter"),
+	FB_PAGES(false, "Facebook Pages");
 	
 	private boolean deprecated;
 	
+	private String displayName;
+	
+	private SocialNetwork(boolean deprecated, String displayName) {
+		this.deprecated = deprecated;
+		this.displayName = displayName;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+
 	public boolean isDeprecated() {
 		return deprecated;
 	}
 	
-	private SocialNetwork(boolean deprecated) {
-		this.deprecated = deprecated;
-	}
+
 }
