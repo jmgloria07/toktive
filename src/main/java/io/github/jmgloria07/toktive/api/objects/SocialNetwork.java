ea22ebd5f1b7324b65cb8ae4cfc25634f6d2b253
jmgloria07 <joshua.gloria07@gmail.com>
2020-09-09 11:53:10 +0800

    Add deprecated property to SocialNetwork enum

--- src/main/java/io/github/jmgloria07/toktive/api/objects/SocialNetwork.java
+++ src/main/java/io/github/jmgloria07/toktive/api/objects/SocialNetwork.java
@@ -1,7 +1,18 @@
 package io.github.jmgloria07.toktive.api.objects;
 
 public enum SocialNetwork {
-	FB,
-	TW,
-	FB_PAGES
+	
+	FB(true),
+	TW(false),
+	FB_PAGES(false);
+	
+	private boolean deprecated;
+	
+	public boolean isDeprecated() {
+		return deprecated;
+	}
+	
+	private SocialNetwork(boolean deprecated) {
+		this.deprecated = deprecated;
+	}
 }
